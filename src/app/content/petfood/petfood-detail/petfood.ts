1048c20cc0f3d7409e8cf150e4ae6a149a011233
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2021-06-17 18:40:03 +0200

    Fix interface

--- src/app/content/petfood/petfood-detail/petfood.ts
+++ src/app/content/petfood/petfood-detail/petfood.ts
@@ -1,8 +1,8 @@
 export interface Petfood {
-    _id: number,
-    animal: string,
-    brand: string,
-    name: string,
+    _id: number
+    animal: string
+    brand: string
+    name: string
     foodType: string
     ingredients: string
 }
