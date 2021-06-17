5c8ac8ee36f90dbe9c01e81268f9394b89f63806
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2021-06-17 19:18:39 +0200

    Update interface

--- src/app/content/petfood/petfood-detail/petfood.ts
+++ src/app/content/petfood/petfood-detail/petfood.ts
@@ -1,8 +1,29 @@
 export interface Petfood {
     _id: number
     animal: string
-    brand: string
-    name: string
     foodType: string
+    source: string
+    brand: string
+    reference: string
+    packaging: string
+    bio: string
+    proteins: number
+    lipids: number
+    fibers: number
+    ashes: number // Cendres
+    moisture: number // Humidité
+    calcium: number
+    phosphorus: number // phosphore (P)
+    omega6: number // N-6
+    omega3: number // N-3
+    starchSource: string // source d'amidon
+    animalProteinSource: string
+    vegetalProteinSource: string
+    monoprotein: boolean
     ingredients: string
+    nutritionalAdditives: string // Additifs additionnels
+    comment: string
+    brandCountryOrigin: string
+    createdAt: Date
+    updatedAt: Date
 }
