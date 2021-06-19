61f43501db2ddd63975b024aafa6e84ddc996dfe
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2021-06-19 15:58:59 +0200

    Add sample images and update mobile display

--- src/app/content/petfood/petfood-list/petfood-outline/petfood-outline.component.ts
+++ src/app/content/petfood/petfood-list/petfood-outline/petfood-outline.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core'
+import { first, shuffle } from 'lodash'
 
 import { Petfood } from '../../petfood-detail'
 
@@ -14,5 +15,16 @@ export class PetfoodOutlineComponent {
 
     getPetfoodUrl(id: number): string[] {
         return ['/petfood', id.toString(), 'detail']
+    }
+
+    getImagePath(): string {
+        const samples = [
+            '0AA721D6E760455A82C482EEB72E9A9E.jpg',
+            '1CA77FC1096A44E2951BBCDF610E78F1.jpg',
+            '1D8C7BA698204BBDAC2D9F712DC8835C.jpg',
+            '3DA48F0607B849F2B3D8A78DF9D51D9A.jpg'
+        ]
+
+        return 'assets/' + first(shuffle(samples))
     }
 }
